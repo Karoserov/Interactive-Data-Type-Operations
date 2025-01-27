5caef78ab983de7bdaf27877ba3bdef6da1119dd
Karoserov <mariyankaroserov@gmail.com>
2025-01-27 17:59:36 +0200

    Add about us page.

--- main.py
+++ main.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
